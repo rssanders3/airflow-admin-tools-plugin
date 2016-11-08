ade7497cccb887752af657d50e282617e0ec8df7
rssanders3 <rssanders3@gmail.com>
2016-11-08 16:45:29 -0700

    Initial verison of the admin tools plugin. Includes what version is deployed, sending alert emails and dispalying the contents of the DAGs Directory. Adding screenshot and filling out README.md

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # airflow-admin-tools-plugin
-An Airflow Plugin that provides a new page to the standard Airflow Web Server to help you perform various operations
+
+## Description
+
+An Apache Airflow (Documentation: https://pythonhosted.org/airflow/, Source Code: https://github.com/apache/incubator-airflow) Plugin that provides a new page to the standard Airflow Web Server to help you perform various operations
+
+## Screenshot
+
+![Alt text](/rssanders3/airflow-admin-tools-plugin/README_images/screenshot.png "Admin Tools Plugin Screenshot")
+
+## How do Deploy
+ 
+1. Copy the admin_tools_plugin.py file and the templates directory into the Airflow Plugins directory
+
+    * The Airflow Plugins Directory is defined in the airflow.cfg file as the variable "plugins_folder"
+    
+    * The Airflow Plugins Directory is, by default, ${AIRFLOW_HOME}/plugins
+    
+    * You may have to create the Airflow Plugins Directory folder as it is not created by default
+ 
+2. Restart the Airflow Services
+
+3. Your done!
---
+++ admin_tools_plugin.py
@@ -0,0 +1,66 @@
+import airflow
+from airflow import configuration
+from airflow.plugins_manager import AirflowPlugin
+
+from flask import flash, request, Blueprint, redirect
+from flask_admin import BaseView, expose
+import logging
+import importlib
+import os
+
+
+class AdminTools(BaseView):
+
+    @expose('/')
+    def index(self):
+        airflow_version = airflow.__version__
+
+        dags_folder = configuration.get('core', 'DAGS_FOLDER')
+
+        dags_folder_contents = []
+        for root, subdirs, files in os.walk(dags_folder):
+            dags_folder_contents.append(root)
+            for file in files:
+                dags_folder_contents.append("\t" + file)
+
+        return self.render("admin_tools_plugin/index.html", airflow_version=airflow_version, dags_folder=dags_folder, dags_folder_contents=dags_folder_contents)
+
+    @expose('/email')
+    def email(self):
+        try:
+            to_email = request.args.get('to_email')
+
+            path, attr = configuration.get('email', 'EMAIL_BACKEND').rsplit('.', 1)
+            logging.info("path: " + str(path))
+            logging.info("attr: " + str(attr))
+
+            module = importlib.import_module(path)
+            logging.info("module: " + str(module))
+
+            backend = getattr(module, attr)
+            backend(to_email, "Test Email", "Test Email", files=None, dryrun=False)
+            flash('Email Sent')
+        except Exception as e:
+            flash('Failed to Send Email: ' + str(e), 'error')
+
+        return redirect("/admin/admintools", code=302)
+
+view = AdminTools(category="Admin", name="Tools")
+
+admin_tools_bp = Blueprint(
+    "admin_tools",
+    __name__,
+    template_folder='templates',
+    static_folder='static',
+    static_url_path='/static/'
+)
+
+
+class AdminToolsPlugin(AirflowPlugin):
+    name = "admin_tools_plugin"
+    operators = []
+    flask_blueprints = [admin_tools_bp]
+    hooks = []
+    executors = []
+    admin_views = [view]
+    menu_links = []
